2ede1093faec0c862e9b9749a033d7ed957a84d1
spacem <spacem>
2020-02-29 19:14:20 +1000

    re-style character selection screen

--- src/views/SelectCharacter.vue
+++ src/views/SelectCharacter.vue
@@ -1,14 +1,38 @@
 <template>
   <div>
     <h1>Select Character</h1>
-    <div v-for="character in characters" :key="character.name">
-      <button @click="selectCharacter(character)">{{ character.name }}</button>
+    <div class="character-list">
+      <div v-for="character in characters" :key="character.name">
+        <button class="character-button" @click="selectCharacter(character)">{{ character.name }}</button>
+      </div>
+      <br />
+      <br />
+      <router-link to="/create-character">Create Character</router-link>
     </div>
-    <router-link to="/login">Log Out</router-link> |
-    <router-link to="/create-character">Create Character</router-link>
+    <div class="character-list-image"></div>
+    <router-link to="/login">Log Out</router-link>
   </div>
 </template>
-
+<style scoped lang="scss">
+.character-list {
+  background-color: burlywood;
+  border: solid 5px black;
+  border-radius: 20px;
+  padding: 15px;
+  float: left;
+  clear: both;
+}
+.character-button {
+  width: 100px;
+}
+.character-list-image {
+  height: 400px;
+  background-image: url("~@/assets/Trees1.jpeg");
+  background-position: top;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+</style>
 <script>
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { mapGetters, mapActions } from "vuex";
